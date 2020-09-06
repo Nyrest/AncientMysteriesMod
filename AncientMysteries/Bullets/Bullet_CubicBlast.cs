465b61ef96646c08d685485844cc4e90a2d148e8
TheLostSoul <TheLostSoul@localhost>
2020-09-06 16:25:09 +0800

    worked on cubic blast's rebound current

--- AncientMysteries/Bullets/Bullet_CubicBlast.cs
+++ AncientMysteries/Bullets/Bullet_CubicBlast.cs
@@ -1,4 +1,5 @@
-﻿using DuckGame;
+﻿using AncientMysteries.AmmoTypes;
+using DuckGame;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,20 @@ namespace AncientMysteries.Bullets
         public Bullet_CubicBlast(float xval, float yval, AmmoType type, float ang = -1, Thing owner = null, bool rbound = false, float distance = -1, bool tracer = false, bool network = true) : base(xval, yval, type, ang, owner, rbound, distance, tracer, network)
         {
 
+        }
+        public override void Update()
+        {
+            base.Update();
+            int count = 15;
+            foreach(Duck d in Level.CheckCircleAll<Duck>(this.position,50))
+            {
+                if (d != DuckNetwork.localConnection.profile.duck && Network.isActive && count >=15)
+                {
+                    count = 0;
+                    Level.Add(new Bullet_Current(this.x,this.y,new AT_Current(),Maths.PointDirection(this.position,d.position),this.owner,true,400,false,true));
+                }
+            }
+            count++;
         }
     }
 }
