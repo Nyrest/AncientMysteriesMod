434145bfb392c52ef76602b89278e5d742d05cb9
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2020-09-20 16:46:58 +0800

    upd

--- AncientMysteries/Miscellaneous/NovaExp.cs
+++ AncientMysteries/Miscellaneous/NovaExp.cs
@@ -18,7 +18,7 @@ namespace AncientMysteries.Items.Miscellaneous
         public float _wait;
         public NovaExp(float xpos, float ypos, bool doWait = true) : base(xpos, ypos)
         {
-            _sprite = this.ReadyToRunMap("crystalExplosion.png", 36, 32);
+            _sprite = this.ReadyToRunMap("crystalExplosionP.png", 36, 36);
             this._sprite.AddAnimation("loop", 1f, false, new int[]
             {
         0,
@@ -39,17 +39,15 @@ namespace AncientMysteries.Items.Miscellaneous
         15,
         16,
         17,
+        18,
             });
             this._sprite.SetAnimation("loop");
             this.graphic = this._sprite;
-            this._sprite.speed = 0.4f + Rando.Float(0.2f);
-            base.xscale = 0.5f + Rando.Float(0.5f);
+            this._sprite.speed = 0.4f;
+            base.xscale = 0.5f;
             base.yscale = base.xscale;
-            this.center = new Vec2(32f, 32f);
-            this._wait = Rando.Float(1f);
-            this._smokeFrame = Rando.Int(1, 3);
+            this.center = new Vec2(18f, 18f);
             base.depth = 1f;
-            this.vSpeed = Rando.Float(-0.2f, -0.4f);
             if (!doWait)
             {
                 this._wait = 0f;
