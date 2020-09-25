286a6f238a61147dfcdabba924617a6ca806d410
Boring3 <boringnight@qq.com>
2020-09-25 20:41:32 +0800

    better logic

--- AncientMysteries/groupNames.cs
+++ AncientMysteries/groupNames.cs
@@ -7,37 +7,35 @@ namespace AncientMysteries
 {
     public static class groupNames
     {
-        public const string topGroupName = "Ancient|";
+        public const string topGroupName = "Ancient";
 
         public const string seriesName = "Items";
 
-        public const string topAndSeries = topGroupName + "|";
+        public const string guns = topGroupName + "|Weapons";
 
-        public const string guns = topGroupName + "Weapons|";
+        public const string equipments = topGroupName + "|Equipments";
 
-        public const string equipments = topGroupName + "Equipments|";
+        public const string e_developer = equipments + "|Developers";
 
-        public const string e_developer = equipments + "Developers";
+        public const string g_rifles = guns + "|Rifles";
 
-        public const string g_rifles = guns + "Rifles";
+        public const string g_shotguns = guns + "|Shotguns";
 
-        public const string g_shotguns = guns + "Shotguns";
+        public const string g_pistols = guns + "|Pistols";
 
-        public const string g_pistols = guns + "Pistols";
+        public const string g_artifacts = guns + "|Artifacts";
 
-        public const string g_artifacts = guns + "Artifacts";
+        public const string g_misc = guns + "|Misc";
 
-        public const string g_misc = guns + "Misc";
+        public const string g_staffs = guns + "|Staffs";
 
-        public const string g_staffs = guns + "Staffs";
+        public const string g_machineGuns = guns + "|Machine Guns";
 
-        public const string g_machineGuns = guns + "Machine Guns";
+        public const string g_isekai = guns + "|Isekai";
 
-        public const string g_isekai = guns + "Isekai";
+        public const string g_melees = guns + "|Melee";
 
-        public const string g_melees = guns + "Melee";
-
-        public const string g_snipers = guns + "Snipers";
+        public const string g_snipers = guns + "|Snipers";
 
         public static class Series
         {
