251e8111c0588b1a995504738fc1cdab5f4df03a
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2020-09-26 16:00:18 +0800

    holy light

---
+++ AncientMysteries/AmmoTypes/AT_Star.cs
@@ -0,0 +1,31 @@
+﻿using AncientMysteries.Bullets;
+using DuckGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AncientMysteries.AmmoTypes
+{
+    public sealed class AT_Star : AmmoType
+    {
+        public AT_Star()
+        {
+            accuracy = 0.2f;
+            range = 1000f;
+            penetration = 1f;
+            rangeVariation = 10f;
+            combustable = true;
+            bulletSpeed = 5f;
+            sprite = TexHelper.ModSprite("holyStar.png");
+            //sprite.CenterOrigin();
+            bulletType = typeof(Bullet_Star);
+        }
+
+        public override Bullet FireBullet(Vec2 position, Thing owner = null, float angle = 0, Thing firedFrom = null)
+        {
+            this.bulletColor = Color.Yellow;
+            return base.FireBullet(position, owner, angle, firedFrom);
+        }
+    }
+}
---
+++ AncientMysteries/Bullets/Bullet_Star.cs
@@ -0,0 +1,44 @@
+﻿using AncientMysteries.AmmoTypes;
+using DuckGame;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AncientMysteries.Bullets
+{
+    public class Bullet_Star : Bullet
+    {
+        private Texture2D _beem;
+
+        private float _thickness;
+
+        public int n = 0;
+
+        public Bullet_Star(float xval, float yval, AmmoType type, float ang = -1, Thing owner = null, bool rbound = false, float distance = -1, bool tracer = false, bool network = true) : base(xval, yval, type, ang, owner, rbound, distance, tracer, network)
+        {
+            _thickness = type.bulletThickness;
+            _beem = this.ModTex2D("holyStar.png");
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            n++;
+            if (n == 5)
+            {
+                Level.Add(new Bullet_Star2(start.x, start.y, new AT_Star2(), _angle + 3.14f, owner, false, 80, false, true));
+                n = 0;
+            }
+            /*foreach (Thing t in Level.CheckCircleAll<Thing>(this.position,10))
+            {
+                if (t != Level.CheckCircleAll<Thing>(DuckNetwork.localConnection.profile.duck.position,20))
+                {
+                    Level.Add(SmallFire.New(t.x, t.y, 0, 0));
+                }
+            }*/
+        }
+
+        public override void OnCollide(Vec2 pos, Thing t, bool willBeStopped)
+        {
+            base.OnCollide(pos, t, willBeStopped);
+        }
+    }
+}
