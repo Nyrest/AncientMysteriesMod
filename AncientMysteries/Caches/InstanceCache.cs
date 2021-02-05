8e5750f54dc86ebf16adbb5feaec26f82ee38e8b
Boring3 <76238985+ImaginaryX@users.noreply.github.com>
2021-02-05 18:48:24 +0800

    FuriousNewYear

---
+++ AncientMysteries/Caches/InstanceCache.cs
@@ -0,0 +1,15 @@
+﻿using AncientMysteries.Helpers;
+using DuckGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries
+{
+    public static class InstanceCache<T> where T : new()
+    {
+        public static readonly T instance = FastNew<T>.CreateInstance();
+    }
+}
