f1f601e5b5fc022c4f8d0f1336c70685c4c904f6
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-02-05 18:48:24 +0800

    FuriousNewYear

---
+++ AncientMysteries/Utilities/AMath2.cs
@@ -0,0 +1,22 @@
+﻿using DuckGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries
+{
+    public static class AMath2
+    {
+        private static readonly Random rand = new Random();
+
+        /// <summary>
+        /// Negative value if random generated number is odd
+        /// </summary>
+        public static float RandomNegative(this float value) =>
+            (rand.Next() & 1) == 0
+                ? value
+                : -value;
+    }
+}
