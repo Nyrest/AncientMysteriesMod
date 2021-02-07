dd2e0388fc841dd036dbe3b5a58457ed449e6029
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2021-02-07 16:27:46 +0800

    a

--- AncientMysteries/Miscellaneous/TempNature.cs
+++ AncientMysteries/Miscellaneous/TempNature.cs
@@ -45,7 +45,6 @@ namespace AncientMysteries.Items.Miscellaneous
             this.center = new Vec2(8.5f, 18f);
             base.depth = 1f;
             t = tOwner;
-            gravity = 0;
             solid = false;
             if (!doWait)
             {
