54691470b2bd76bf6334274ebc3363dd1ecd581a
Boring3 <76238985+ImaginaryX@users.noreply.github.com>
2021-02-07 10:34:18 +0800

    ah

---
+++ AncientMysteries/Items/FutureTech/Grenades/AtomicReboundShield.cs
@@ -0,0 +1,35 @@
+﻿using AncientMysteries.Localization.Enums;
+using DuckGame;
+using static AncientMysteries.groupNames;
+
+namespace AncientMysteries.Items.FutureTech.Grenades
+{
+    [EditorGroup(guns)]
+    public class ReboundShield : AMGun, IPlatform
+    {
+        public override string GetLocalizedName(AMLang lang) => lang switch
+        {
+            _ => "Rebound Shield",
+        };
+
+        public ReboundShield(float xval, float yval) : base(xval, yval)
+        {
+            ammo = 1;
+            var _sprite = new SpriteMap("rock01", 16, 16);
+            graphic = _sprite;
+            center = new Vec2(8f, 8f);
+            collisionOffset = new Vec2(-8f, -5f);
+            collisionSize = new Vec2(16f, 12f);
+            thickness = 100f;
+            weight = 10f;
+            flammable = 0f;
+            physicsMaterial = PhysicsMaterial.Metal;
+        }
+
+        public override void ApplyKick() { }
+
+        public override void PressAction() { }
+
+        public override void Fire() { }
+    }
+}
--- AncientMysteries/Utilities/Rand.cs
+++ AncientMysteries/Utilities/Rand.cs
@@ -2,21 +2,24 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AncientMysteries
 {
-    public static class AMath2
+    public static class Rand
     {
         private static readonly Random rand = new Random();
 
         /// <summary>
         /// Negative value if random generated number is odd
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float RandomNegative(this float value) =>
-            (rand.Next() & 1) == 0
-                ? value
-                : -value;
+        (rand.Next() & 1) == 0
+            ? value
+            : -value;
     }
 }
