a1f6093c2d8cff3454d5a2797c0c6d1854df36ff
Boring3 <16686147+BThree496@users.noreply.github.com>
2021-02-10 12:02:54 +0800

    SourceGenerator NetFx Test

---
+++ AncientMysteries.SourceGenerator/_BaseGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace AncientMysteries.SourceGenerator
+{
+    public abstract class _BaseGenerator : ISourceGenerator
+    {
+        public abstract string UniqueName { get; }
+
+        public abstract void Execute(GeneratorExecutionContext context);
+
+        public abstract void Initialize(GeneratorInitializationContext context);
+    }
+}
---
+++ AncientMysteries.SourceGenerator/_BaseSourceGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AncientMysteries.SourceGenerator
+{
+    public abstract class _BaseSourceGenerator : _BaseGenerator
+    {
+        public override void Execute(GeneratorExecutionContext context)
+        {
+            var contentBuilder = SBPool.Rent();
+            Generate(context, contentBuilder);
+            string source = @$"
+{_CompileSettings.DefaultUsing}
+namespace {_CompileSettings.Namespace}
+{{
+    {contentBuilder.ToString()}
+}}
+";
+            context.AddSource(UniqueName + ".cs", SourceText.From(source, Encoding.UTF8));
+        }
+
+        public abstract void Generate(GeneratorExecutionContext context, StringBuilder sb);
+
+        public override void Initialize(GeneratorInitializationContext context) { }
+    }
+}
