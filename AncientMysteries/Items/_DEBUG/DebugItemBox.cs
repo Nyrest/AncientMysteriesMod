2a548fe23dd58fa477ec128a0e6d1c6f37761fc1
Boring3 <16686147+BThree496@users.noreply.github.com>
2021-02-16 23:11:50 +0800

    fix

--- AncientMysteries/Items/_DEBUG/DebugItemBox.cs
+++ AncientMysteries/Items/_DEBUG/DebugItemBox.cs
@@ -13,15 +13,25 @@ namespace AncientMysteries.Items._DEBUG
     public class DebugItemBox : ItemSpawner
     {
         public static Type[] amTypes = Assembly.GetExecutingAssembly().GetTypes().Where(x => typeof(Holdable).IsAssignableFrom(x)).ToArray();
+        public static List<TypeProbPair> probPairs = new List<TypeProbPair>(amTypes.Select(x => new TypeProbPair() { probability = 1, type = x }));
+        public static FieldInfo fuck = typeof(ItemSpawner).GetField("_possible", BindingFlags.NonPublic | BindingFlags.Instance);
 
         public DebugItemBox(float xpos, float ypos, Type c = null) : base(xpos, ypos, c)
         {
             _editorName = "|DGPURPLE|Debug Item Box";
             randomSpawn = true;
+            fuck.SetValue(this, probPairs);
+        }
+        public override void DoInitialize()
+        {
+            fuck.SetValue(this, probPairs);
+            base.DoInitialize();
+            fuck.SetValue(this, probPairs);
         }
 
         public override void Update()
         {
+            fuck.SetValue(this, probPairs);
             base.Update();
             contains = amTypes[Rando.Int(amTypes.Length - 1)];
         }
