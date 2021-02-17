5a113986a8e26f23efda25f26124eabd0518dc8a
Boring3 <16686147+BThree496@users.noreply.github.com>
2021-02-17 13:53:40 +0800

    Random Item

---
+++ AncientMysteries/Items/_DEBUG/RandomAMItem.cs
@@ -0,0 +1,41 @@
+﻿using DuckGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static AncientMysteries.groupNames;
+
+namespace AncientMysteries.Items._DEBUG
+{
+    [EditorGroup(_debugGroup)]
+    public class RandomAMItem : Holdable
+    {
+        public static Type[] amTypes = Assembly.GetExecutingAssembly().GetTypes().Where(x => !x.IsAbstract && typeof(Holdable).IsAssignableFrom(x)).ToArray();
+
+        public RandomAMItem()
+        {
+            this.ReadyToRun(Texs.HatBoring3Pickup);
+            _editorName = "|DGPURPLE|Random";
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            if (Level.current is Editor || (Network.isActive && !this.isServerForObject)) return;
+            var fuckingType = amTypes[Rando.Int(amTypes.Length - 1)];
+            if (Editor.CreateThing(fuckingType) is PhysicsObject newThing)
+            {
+                newThing.x = base.x;
+                newThing.y = this.y;
+                newThing.vSpeed = -2f;
+                newThing.spawnAnimation = true;
+                newThing.isSpawned = true;
+                newThing.offDir = offDir;
+                Level.Add(newThing);
+            }
+            Level.Remove(this);
+        }
+    }
+}
