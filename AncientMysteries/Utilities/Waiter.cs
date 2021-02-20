56ad8965ad0de30d227972a0f12d1b75f2c1e764
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-02-20 18:26:57 +0800

    upd

---
+++ AncientMysteries/Utilities/Waiter.cs
@@ -0,0 +1,27 @@
+﻿using AncientMysteries.Utilities.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Utilities
+{
+    public static class Waiter
+    {
+        public static DictSlim<string, int> dict = new DictSlim<string, int>();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool WaitUnique<T>(
+            this T obj,
+            int totalFramesToWait,
+            [CallerFilePath] string path = null,
+            [CallerMemberName] string member = null,
+            [CallerLineNumber] int line = -1)
+        {
+            ref int currentFrame = ref dict.GetOrAddValueRef(string.Concat(obj.GetHashCode(), path, member, line.ToString()));
+            return currentFrame++ >= totalFramesToWait;
+        }
+    }
+}
