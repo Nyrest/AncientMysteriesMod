afb5feee2c761b614b7c3442220c231502952bce
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-03 23:14:10 +0800

    upd

--- AncientMysteries/Utilities/Make.cs
+++ AncientMysteries/Utilities/Make.cs
@@ -5,12 +5,16 @@ namespace AncientMysteries.Utilities
     public static class Make
     {
         #region Bullet
-        public static Bullet Bullet<TAmmoType>(Vec2 position, Thing owner = null, float angle = 0, Thing firedFrom = null) where TAmmoType : AmmoType, new()
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Bullet Bullet<TAmmoType>(Vec2 position, Thing owner = null, float angleDegress = 0, Thing firedFrom = null) 
+            where TAmmoType : AmmoType, new()
         {
             //var ammoType = FastNew<TAmmoType>.CreateInstance();
             var ammoType = InstanceOf<TAmmoType>.instance;
-            return ammoType.FireBullet(position, owner, angle, firedFrom);
+            return ammoType.FireBullet(position, owner, angleDegress, firedFrom);
         }
+
+
         #endregion
     }
 }
