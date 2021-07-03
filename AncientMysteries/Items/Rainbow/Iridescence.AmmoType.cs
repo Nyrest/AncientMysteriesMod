7b44f47ce229ed3cc88075054984268838addb73
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-03 22:23:30 +0800

    upd

---
+++ AncientMysteries/Items/Rainbow/Iridescence.AmmoType.cs
@@ -0,0 +1,17 @@
+﻿namespace AncientMysteries.Items.Rainbow
+{
+    public sealed class Iridescence_AmmoType : AmmoType
+    {
+        public Iridescence_AmmoType()
+        {
+            accuracy = 1f;
+            range = 800f;
+            penetration = 2f;
+            rangeVariation = 10f;
+            bulletLength = 3000;
+            combustable = true;
+            this.bulletType = typeof(Iridescence_Bullet);
+            bulletColor = Color.White;
+        }
+    }
+}
---
+++ AncientMysteries/Items/Rainbow/Iridescence.Bullet.cs
@@ -0,0 +1,16 @@
+﻿namespace AncientMysteries.Items.Rainbow
+{
+    public class Iridescence_Bullet : Bullet
+    {
+        public Iridescence_Bullet(float xval, float yval, AmmoType type, float ang = -1, Thing owner = null, bool rbound = false, float distance = -1, bool tracer = false, bool network = true) : base(xval, yval, type, ang, owner, rbound, distance, tracer, network) { }
+
+        public override void Update()
+        {
+            base.Update();
+            if (color == Color.White)
+            {
+                color = HSL.FromHslFloat(Rando.Float(0f, 1f), Rando.Float(0.7f, 1f), Rando.Float(0.45f, 0.65f));
+            }
+        }
+    }
+}
