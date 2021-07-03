60b5a10a2248f450c31734de3693b57c4d768715
TheLostSoul <30169665+Ghogm4@users.noreply.github.com>
2021-07-03 22:18:07 +0800

    recovered green and made yellow

---
+++ AncientMysteries/Bullets/Bullet_Flowerr.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using AncientMysteries.Items.Miscellaneous;
+
+namespace AncientMysteries.Bullets
+{
+    public class Bullet_Flowerr : Bullet
+    {
+        public Bullet_Flowerr(float xval, float yval, AmmoType type, float ang = -1, Thing owner = null, bool rbound = false, float distance = -1, bool tracer = false, bool network = true) : base(xval, yval, type, ang, owner, rbound, distance, tracer, network)
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+        public override void Removed()
+        {
+            var firedBullets = new List<Bullet>(5);
+            for (int i = 0; i < 5; i++)
+            {
+                var bullet = Make.Bullet<AT_Leaf>(travelEnd, owner, Rando.Float(0f, 360f), this);
+                firedBullets.Add(bullet);
+                Level.Add(bullet);
+            }
+            if (Network.isActive)
+            {
+                NMFireGun gunEvent = new(null, firedBullets, (byte)firedBullets.Count, rel: false, 4);
+                Send.Message(gunEvent, NetMessagePriority.ReliableOrdered);
+                firedBullets.Clear();
+            }
+            base.Removed();
+        }
+    }
+}
