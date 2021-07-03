61b02f0355f049b0f2a30c9af5f808ceff490aad
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-03 15:28:38 +0800

    Ticker

---
+++ AncientMysteries/Utilities/Ticker.cs
@@ -0,0 +1,23 @@
+﻿namespace AncientMysteries.Utilities
+{
+    public sealed class Ticker
+    {
+        public readonly uint totalUpdateCount;
+        public uint currentUpdateCount;
+
+        public Ticker(uint totalUpdateCount)
+        {
+            this.totalUpdateCount = totalUpdateCount;
+        }
+
+        public bool Tick()
+        {
+            if (currentUpdateCount++ == totalUpdateCount)
+            {
+                currentUpdateCount = 0;
+                return true;
+            }
+            return false;
+        }
+    }
+}
