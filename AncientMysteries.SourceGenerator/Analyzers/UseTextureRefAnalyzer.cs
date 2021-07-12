557549926dce76619caf9af10673648e7bb84a40
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-12 16:16:24 +0800

    Add Analyzer

---
+++ AncientMysteries.SourceGenerator/Analyzers/UseTextureRefAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using AncientMysteries.SourceGenerator.Generators;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AncientMysteries.SourceGenerator.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UseTextureRefAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AM0001";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Res.Analyzer_UseTextureRef_Title), Res.ResourceManager, typeof(Res));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Res.Analyzer_UseTextureRef_MessageFormat), Res.ResourceManager, typeof(Res));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Res.Analyzer_UseTextureRef_Description), Res.ResourceManager, typeof(Res));
+        private const string Category = "Usage";
+
+        private static DiagnosticDescriptor Rule = new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.StringLiteralExpression);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var syntax = (LiteralExpressionSyntax)context.Node;
+            var a = syntax.GetText();
+            string text = a.ToString(new TextSpan(a.Length - 5, 5));
+            if (text.Equals(".png\"", StringComparison.OrdinalIgnoreCase))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation(), TexturesReference.GetFieldName(a.ToString(new TextSpan(1, a.Length - 2)))));
+            }
+        }
+    }
+}
--- AncientMysteries.SourceGenerator/Generators/TexturesReference.cs
+++ AncientMysteries.SourceGenerator/Generators/TexturesReference.cs
@@ -1,7 +1,7 @@
 ﻿using System.IO;
 using System.Collections.Generic;
 
-namespace AncientMysteries.SourceGenerator.Generatorsh
+namespace AncientMysteries.SourceGenerator.Generators
 {
     [Generator]
     public class TexturesReference : _BaseSourceGenerator
@@ -22,18 +22,25 @@ namespace AncientMysteries.SourceGenerator.Generatorsh
                 if (Path.GetExtension(fullname).Equals(".png", StringComparison.OrdinalIgnoreCase))
                 {
                     string filename = Path.GetFileName(fullname);
-                    string filenameNoExt = Path.GetFileNameWithoutExtension(fullname);
-                    string fieldName = char.ToUpperInvariant(filenameNoExt[0]) + filenameNoExt.Substring(1).Replace(" ", null);
-                    if (char.IsNumber(fieldName[0]))
-                        fieldName = "_" + fieldName;
-                    sb.AppendLine(TabLevel(2) + $"public const string t_{fieldName} = \"{filename}\";");
-                    allTexturesBuilder.Append($"\n{TabLevel(2)}t_{fieldName},");
+                    string fieldName = GetFieldName(filename);
+                    sb.AppendLine(TabLevel(2) + $"public const string {fieldName} = \"{filename}\";");
+                    allTexturesBuilder.Append($"\n{TabLevel(2)}{fieldName},");
                 }
             }
             allTexturesBuilder.AppendLine($"\n{TabLevel(2)}}};");
             sb.Append(allTexturesBuilder);
             sb.Append(TabLevel(1));
             sb.AppendLine("}");
+        }
+
+        //TODO: Optimize
+        public static string GetFieldName(string filename)
+        {
+            string filenameNoExt = Path.GetFileNameWithoutExtension(filename);
+            string fieldName = char.ToUpperInvariant(filenameNoExt[0]) + filenameNoExt.Substring(1).Replace(" ", null);
+            if (char.IsNumber(fieldName[0]))
+                fieldName = "_" + fieldName;
+            return "t_" + fieldName;
         }
     }
 }
