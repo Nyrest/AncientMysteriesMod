6cd8a07c1fabfc987b24599e3d74ad9d0c9c1ebc
Erstori <16686147+Erstori@users.noreply.github.com>
2021-07-13 00:27:34 +0800

    Update Hats

--- AncientMysteries/Items/Developers/Hats/GUDZH.cs
+++ AncientMysteries/Items/Developers/Hats/GUDZH.cs
@@ -5,19 +5,49 @@ namespace AncientMysteries.Items.Developers.Hats
     [EditorGroup(e_developer)]
     public sealed class GUDZH : AMHelmet
     {
+        
+
         public GUDZH(float xpos, float ypos) : base(xpos, ypos)
         {
             _sprite = this.ModSpriteMap(t_HatGUDZH, 32, 32, true);
             _pickupSprite = this.ReadyToRun(t_HatGUDZHPickup);
+            _sprite.CenterOrigin();
             EquipmentMaxHitPoints = 32767;
             EquipmentHitPoints = 32767;
             _isArmor = true;
             _equippedThickness = int.MaxValue;
         }
 
-        public override string GetLocalizedName(AMLang lang)
+        public override void Update()
         {
-            throw new NotImplementedException();
+            base.Update();
+            this.scale = Vec2.One;
+            if (owner is Duck duck)
+            {
+                if (duck.quack != 0)
+                {
+                    this.scale = new Vec2(2);
+                }
+            }
         }
+
+        public override bool Hit(Bullet bullet, Vec2 hitPos)
+        {
+            if (_equippedDuck == null || bullet.owner == duck || !bullet.isLocal)
+                return false;
+
+            if (bullet.ammo is null) return false;
+            var result = base.Hit(bullet, hitPos);
+            if (result)
+            {
+                bullet.ReverseTravel();
+            }
+            return result;
+        }
+
+        public override string GetLocalizedName(AMLang lang) => lang switch
+        {
+            _ => "GUDZH",
+        };
     }
 }
