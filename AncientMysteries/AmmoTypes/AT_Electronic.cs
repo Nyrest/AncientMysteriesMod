f70091a0a5deda1b28fa0da0fc8cfb74b4786425
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-13 20:20:52 +0800

    upd

--- AncientMysteries/AmmoTypes/AT_Electronic.cs
+++ AncientMysteries/AmmoTypes/AT_Electronic.cs
@@ -10,18 +10,10 @@
             rangeVariation = 20f;
             bulletThickness = 2f;
             bulletColor = Color.Lime;
-            bulletType = typeof(Bullet_Electronic);
-            bulletSpeed = 30f;
-            //this.bulletType = typeof(Bullet_Electronic);
+            bulletSpeed = 40f;
             //this.sprite = TexHelper.ModSprite("ElectronicStar.png");
             //this.sprite.CenterOrigin();
             //bulletLength = 10;
-        }
-
-        public override Bullet FireBullet(Vec2 position, Thing owner = null, float angle = 0, Thing firedFrom = null)
-        {
-            bulletColor = Color.Lime;
-            return base.FireBullet(position, owner, angle, firedFrom);
         }
     }
 }
