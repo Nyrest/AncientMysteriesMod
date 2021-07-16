4404f68f1f516bd06ef0ca4cd5443e3973c160f2
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-16 11:19:39 +0800

    arcane nova

--- AncientMysteries/Items/Staffs/ArcaneNova.Magic.Stage1.cs
+++ AncientMysteries/Items/Staffs/ArcaneNova.Magic.Stage1.cs
@@ -1,16 +1,45 @@
-﻿namespace AncientMysteries.Items.Staffs
+﻿using AncientMysteries.DestroyTypes;
+using AncientMysteries.Items.Miscellaneous;
+
+namespace AncientMysteries.Items.Staffs
 {
-    public class ArcaneNova_Magic_Stage1 : AMThing
+    public class ArcaneNova_Magic_Stage1 : AMThingBulletLinar
     {
-        public StateBinding _positionBinding = new CompressedVec2Binding(nameof(position), int.MaxValue, isvelocity: false, doLerp: true);
-
-        public StateBinding _travelBinding = new CompressedVec2Binding(nameof(_travelAngleRadius), 20);
-
-        private float _travelAngleRadius;
-
-        public override void OnGhostObjectAdded()
+        public ArcaneNova_Magic_Stage1(Vec2 pos, Vec2 initSpeed, Duck safeDuck) : base(pos, 275f, float.PositiveInfinity, initSpeed, safeDuck)
         {
-            base.OnGhostObjectAdded();
+            var _spriteMap = t_Bullet_NovaFrame.ModSpriteWithFrames(32, 32, true);
+            _spriteMap.AddAnimation("loop", 0.15f, true, 0, 1, 2, 3);
+            _spriteMap.SetAnimation("loop");
+            _spriteMap.CenterOrigin();
+            graphic = _spriteMap;
+        }
+
+        public override void BulletRemove()
+        {
+            base.BulletRemove();
+            NovaExp n = new(x, y, true);
+            n.xscale *= 3f;
+            n.yscale *= 3f;
+            Level.Add(n);
+            SFX.Play("explode", 0.8f, Rando.Float(-0.1f, 1f), 0f, false);
+
+            if (isServerForObject)
+            {
+                IEnumerable<MaterialThing> things = Level.CheckCircleAll<MaterialThing>(position, 32f);
+                foreach (MaterialThing t2 in things)
+                {
+                    if (t2 != owner)
+                    {
+                        t2.Destroy(new DT_ThingBullet(this));
+                    }
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    var bullet = new ArcaneNova_Magic_Stage2(position, Maths.AngleToVec(Rando.Float(0, Maths.PI)) * 6, BulletSafeDuck);
+                    Level.Add(bullet);
+                }
+            }
         }
     }
 }
