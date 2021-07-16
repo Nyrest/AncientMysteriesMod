91c5ac065230ab391ecfc306770b00f5e981195d
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2021-07-16 23:22:21 +0800

    a

---
+++ AncientMysteries/Items/Staffs/PrimordialLibram.ThingBullet.Flower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items.Staffs
+{
+    public class PrimordialLibram_ThingBullet_Flower : AMThingBulletLinar
+    {
+        public PrimordialLibram_ThingBullet_Flower(Vec2 pos, Vec2 initSpeed, Duck safeDuck) : base(pos, 300, 1, initSpeed, safeDuck)
+        {
+            this.ReadyToRun(t_Bullet_Flower);
+        }
+
+        public 
+    }
+}
