6d5e5040967cf39b12cb20f27f009e3d5a9b1028
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2021-07-16 16:50:24 +0800

    lol

---
+++ AncientMysteries/Items/Staffs/HolyLight.ThingBullet2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items.Staffs
+{
+    public class HolyLight_ThingBullet2 : AMThingBulletLinar
+    {
+        public HolyLight_ThingBullet2(Vec2 pos, Vec2 initSpeed, Duck safeDuck) : base(pos, 0.2f, int.MaxValue, initSpeed, safeDuck)
+        {
+            this.ReadyToRun(t_Bullet_HolyStar2);
+            alpha = Rando.Float(0.7f, 1);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
