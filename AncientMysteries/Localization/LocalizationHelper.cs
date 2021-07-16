b08aed3ff96faf7702ff11f8915ab919a050761c
Erstori <16686147+Erstori@users.noreply.github.com>
2021-07-16 22:51:24 +0800

    upd

---
+++ AncientMysteries/Localization/LocalizationHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Localization
+{
+    public static class LocalizationHelper
+    {
+        public static AMLang DefaultLang => AMLang.english;
+    }
+}
---
+++ AncientMysteries/NativeMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries
+{
+    public static class NativeMethods
+    {
+        #region MyRegion
+        [DllImport("steam_api.dll", EntryPoint = "SteamAPI_ISteamUtils_GetSteamUILanguage")]
+        private static extern IntPtr SteamAPI_ISteamUtils_GetSteamUILanguage(IntPtr c_instancePtr);
+        #endregion
+    }
+}
