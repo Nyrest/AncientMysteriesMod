77008cde3c2a574bbb35d2b3eaadcef47e78e085
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-17 20:44:54 +0800

    upd

---
+++ AncientMysteries/Bases/AMEquipmentWing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Bases
+{
+    public abstract class AMEquipmentWing : AMEquipment
+    {
+        protected AMEquipmentWing(float xpos, float ypos) : base(xpos, ypos)
+        {
+        }
+    }
+}
--- AncientMysteries/Items/Equipments/Back/FallenAngel.cs
+++ AncientMysteries/Items/Equipments/Back/FallenAngel.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace AncientMysteries.Items.Equipments.Back
 {
-    class FallenAngel
+    [EditorGroup(e_developer)]
+#warning Texture Todo
+    [MetaImage(t_Bullet_NovaFrame)]
+    [MetaInfo(Lang.english, "Fallen Angel", "感謝と祈りを忘れぬ限り、神は我々をお救い下さいます　私とともに、祈りを捧げましょう.")]
+    [MetaInfo(Lang.schinese, "堕落天使", null)]
+    public partial class FallenAngel : AMEquipmentWing
     {
+        public FallenAngel(float xpos, float ypos) : base(xpos, ypos)
+        {
+        }
     }
 }
