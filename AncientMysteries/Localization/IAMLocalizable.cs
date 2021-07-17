0c260d11a52081a797d6f3fa208408fe4fa411c7
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-17 20:30:39 +0800

    update

--- AncientMysteries/Localization/IAMLocalizable.cs
+++ AncientMysteries/Localization/IAMLocalizable.cs
@@ -5,8 +5,8 @@
     /// </summary>
     public interface IAMLocalizable
     {
-        public string GetLocalizedName(Lang lang);
+        //public string GetLocalizedName(Lang lang);
 
-        public string GetLocalizedDescription(Lang lang);
+        //public string GetLocalizedDescription(Lang lang);
     }
 }
