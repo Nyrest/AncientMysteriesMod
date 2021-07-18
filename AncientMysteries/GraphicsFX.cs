c2b22b634c319b26f0b70f09553c171e4b9d2b13
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-18 23:03:20 +0800

    Effects

---
+++ AncientMysteries/GraphicsFX.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace AncientMysteries
+{
+    public static class GfxEffects
+    {
+        #region Passive Reverser Bindings
+        // Avoid bugs cause permanent graphics effects
+        private static readonly Dictionary<Func<bool>, Action> passiveReverserBindings = new();
+        private static readonly List<Func<bool>> passiveReverserBindingsToRemove = new();
+        #endregion
+
+        #region Defaults
+        private static readonly BlendState DefaultBlendState = Graphics.device.BlendState;
+        private static readonly Color DefaultBlendFactor = Graphics.device.BlendFactor;
+        #endregion
+
+        #region Effect Invert Color
+        public static void EnableInvertColor(Thing binder) => EnableInvertColor(() => binder.removeFromLevel);
+
+        public static void EnableInvertColor(Func<bool> binder)
+        {
+            passiveReverserBindings.Add(binder, DisableInvertColor);
+            Graphics.device.BlendState.ColorDestinationBlend = Blend.InverseSourceColor;
+        }
+
+        public static void DisableInvertColor()
+        {
+            Graphics.device.BlendState.ColorDestinationBlend = DefaultBlendState.ColorDestinationBlend;
+        }
+        #endregion
+
+        #region Effect Blend Factor
+        public static void EnableBlendFactor(Thing binder, Color factor) => EnableBlendFactor(() => binder.removeFromLevel, factor);
+
+        public static void EnableBlendFactor(Func<bool> binder, Color factor)
+        {
+            passiveReverserBindings.Add(binder, DisableBlendFactor);
+            Graphics.device.BlendFactor = factor;
+        }
+
+        public static void DisableBlendFactor()
+        {
+            Graphics.device.BlendFactor = DefaultBlendFactor;
+        }
+        #endregion
+
+        public static void Update()
+        {
+            foreach (var passive in passiveReverserBindings)
+            {
+                if (passive.Key())
+                {
+                    passiveReverserBindingsToRemove.Add(passive.Key);
+                }
+            }
+            #region MyRegion
+            if (passiveReverserBindingsToRemove.Count != 0)
+            {
+                foreach (var item in passiveReverserBindingsToRemove)
+                {
+                    passiveReverserBindings.Remove(item);
+                }
+                passiveReverserBindingsToRemove.Clear();
+            }
+            #endregion
+        }
+    }
+}
