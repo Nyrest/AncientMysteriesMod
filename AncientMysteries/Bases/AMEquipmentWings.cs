9694b433a01a21a2a9fddd7059fd9d4a3524b3ca
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-19 11:02:14 +0800

    wings

--- AncientMysteries/Bases/AMEquipmentWings.cs
+++ AncientMysteries/Bases/AMEquipmentWings.cs
@@ -26,6 +26,16 @@
         public override void Update()
         {
             base.Update();
+            if (this._equippedDuck is not Duck _equippedDuck) return;
+            if (!_equippedDuck.grounded && _equippedDuck.InputDown(trigger_Jump))
+            {
+                isFlying = true;
+            }
+            else if(_equippedDuck.grounded)
+            {
+                isFlying = false;
+            }
+
             _wingsSpriteMap.SetAnimation(isFlying ? "loop" : "idle");
             if (_equippedDuck is Duck equippedDuck)
             {
@@ -36,6 +46,23 @@
             {
                 timeFlied = 0;
             }
+
+            if (isFlying)
+            {
+                PhysicsObject propel = _equippedDuck;
+                if (_equippedDuck._trapped != null)
+                {
+                    propel = _equippedDuck._trapped;
+                }
+                else if (_equippedDuck.ragdoll?.part1 != null)
+                {
+                    propel = _equippedDuck.ragdoll.part1;
+                }
+                propel.position += GetFlyDir() * 3.5f;
+                propel.velocity = Vec2.Zero;
+                _equippedDuck.gravMultiplier = 0;
+            }
+            _equippedDuck.gravMultiplier = 1;
         }
 
         public Vec2 GetFlyDir()
@@ -46,20 +73,20 @@
             }
             else
             {
-                Vec2 dir = new Vec2(0f, 0f);
-                if (_equippedDuck.inputProfile.Down("LEFT"))
+                Vec2 dir = new(0f, 0f);
+                if (_equippedDuck.InputDown(trigger_Left))
                 {
                     dir.x -= 1f;
                 }
-                if (_equippedDuck.inputProfile.Down("RIGHT"))
+                if (_equippedDuck.InputDown(trigger_Right))
                 {
                     dir.x += 1f;
                 }
-                if (_equippedDuck.inputProfile.Down("UP"))
+                if (_equippedDuck.InputDown(trigger_Up) || _equippedDuck.InputDown(trigger_Jump))
                 {
                     dir.y -= 1f;
                 }
-                if (_equippedDuck.inputProfile.Down("DOWN"))
+                if (_equippedDuck.InputDown(trigger_Down))
                 {
                     dir.y += 1f;
                 }
--- AncientMysteries/Items/Equipments/Back/AngelWings.cs
+++ AncientMysteries/Items/Equipments/Back/AngelWings.cs
@@ -2,14 +2,14 @@
 {
     [EditorGroup(group_Equipments)]
     [MetaImage(tex_Equipment_DemonWings, 28, 14, 1)]
-    [MetaInfo(Lang.english, "Angel Wings", "「あぁ〜麻婆豆腐〜♪〜♪」")]
+    [MetaInfo(Lang.english, "Angel Wings", "「また 60 億分の 1 の確率で出会えたら・・・」")]
     [MetaInfo(Lang.schinese, "天使之翼", null)]
     public partial class AngelWings : AMEquipmentWings
     {
         public AngelWings(float xpos, float ypos) : base(xpos, ypos)
         {
             _wingsSpriteMap = this.ReadyToRunWithFrames(tex_Equipment_DemonWings, 28, 14);
-            _wingsSpriteMap.AddAnimation("loop", 0.18f, true, 0, 1, 2, 1);
+            _wingsSpriteMap.AddAnimation("loop", 0.15f, true, 0, 1, 2, 1);
             _wingsSpriteMap.AddAnimation("idle", 1f, true, 0);
             wearOffset = new(-0.5f, -1);
             _equippedDepth = -2;
@@ -24,7 +24,7 @@
 
         public override string GetLocalizedDescription(Lang lang) => lang switch
         {
-            _ => "「あぁ〜麻婆豆腐〜♪〜♪」",
+            _ => "「また 60 億分の 1 の確率で出会えたら・・・」",
         };
 
         public override void Update()
@@ -32,33 +32,20 @@
             base.Update();
             if (this._equippedDuck is not Duck _equippedDuck) return;
 
-            if (_equippedDuck.grounded && _equippedDuck.inputProfile.Down("JUMP"))
-            {
-                
-            }
 
-            PhysicsObject propel = _equippedDuck;
-            if (_equippedDuck._trapped != null)
-            {
-                propel = _equippedDuck._trapped;
-            }
-            else if (_equippedDuck.ragdoll?.part1 != null)
-            {
-                propel = _equippedDuck.ragdoll.part1;
-            }
-            propel.velocity += GetFlyDir() * 1.5f;
         }
 
         public override void Equip(Duck d)
         {
             base.Equip(d);
-            d.gravMultiplier = 0.2f;
+            //_equippedDuck.gravMultiplier = 0.8f;
         }
 
         public override void UnEquip()
         {
+            if (_equippedDuck is not null)
+                _equippedDuck.gravMultiplier = 1f;
             base.UnEquip();
-            d.gravMultiplier = 1f;
         }
     }
 }
