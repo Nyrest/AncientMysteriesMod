eb1254a1f7be93d5174cd89cdab196efdce0e3da
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-20 19:19:25 +0800

    6s

--- AncientMysteries/Buffs/Paralyzed.cs
+++ AncientMysteries/Buffs/Paralyzed.cs
@@ -8,7 +8,7 @@ namespace AncientMysteries.Buffs
 {
     public partial class Paralyzed : Equipment
     {
-        public Waiter waiter = new Waiter(30000);
+        public Waiter waiter = new Waiter(360);
         public Paralyzed(float xpos, float ypos) : base(xpos, ypos)
         {
             
