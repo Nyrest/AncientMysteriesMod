4581703813f0f14ee8c6a5f9178a3e2a049616d8
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-20 19:17:33 +0800

    Point Light

--- AncientMysteries/Bases/AMDecoration.cs
+++ AncientMysteries/Bases/AMDecoration.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace AncientMysteries.Items
 {
-    public abstract class AMDecoration : AMThing
+    public abstract class AMDecoration : AMThing, IAMLocalizable
     {
-        protected AMDecoration()
+        protected AMDecoration(float xpos, float ypos) : base(xpos, ypos)
         {
         }
+
+        public virtual string GetLocalizedDescription(Lang lang) => string.Empty;
+        public virtual string GetLocalizedName(Lang lang) => string.Empty;
     }
 }
