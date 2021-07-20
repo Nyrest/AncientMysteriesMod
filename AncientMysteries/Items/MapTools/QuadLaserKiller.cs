fad101115ebb59a592b21c448b0aa3b452715080
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-20 20:46:43 +0800

    make the world better

--- AncientMysteries/Items/MapTools/QuadLaserKiller.cs
+++ AncientMysteries/Items/MapTools/QuadLaserKiller.cs
@@ -1,37 +1,41 @@
 ﻿using System;
 
-namespace DuckGame
+namespace AncientMysteries.Items
 {
-    [EditorGroup(group_ModTools)]
-    public class QuadLaserKiller : CustomCamera
+    [EditorGroup(group_MapTools)]
+    [MetaImage(tex_MapTools_Swirl)]
+    [MetaInfo(Lang.english, "Quad Laser Killer", "Perfect for those who wants to kill laggy things.")]
+    [MetaInfo(Lang.schinese, null, "给那些想要手动移除 QuadLaserBullet 的人的礼物")]
+    [MetaType(MetaType.MapTools)]
+    public partial class QuadLaserKiller : AMMapTool
     {
-        public EditorProperty<int> Size = new EditorProperty<int>(320, null, 60f, 1920f, 1f);
-
-        public QuadLaserKiller()
+        public EditorProperty<float> Width = new EditorProperty<float>(100, null, 1f, 1000f, 1f)
         {
-            _contextMenuFilter.Add("wide");
+            name = "Width",
+            _tooltip = "Width"
+        };
+
+        public EditorProperty<float> Height = new EditorProperty<float>(100, null, 1f, 1000f, 1f)
+        {
+            name = "Height",
+            _tooltip = "Height"
+        };
+
+        public QuadLaserKiller(float xpos, float ypos) : base(xpos, ypos)
+        {
+            this.ReadyToRun(tex_MapTools_Swirl);
             _editorName = "Quadlaser Killer";
             editorTooltip = "Perfect for those who wants to kill laggy things.";
-            Size._tooltip = "The size of killing quad lasers (in pixel)";
-            graphic = new Sprite("cameraIcon");
-            collisionSize = new Vec2(8f, 8f);
-            collisionOffset = new Vec2(-4f, -4f);
             _visibleInGame = false;
-        }
-
-        public override void Initialize()
-        {
-            wide.value = Size.value;
-            base.Initialize();
         }
 
         public override void Update()
         {
-            if (!(Level.current is GameLevel) || GameMode.started)
+            if (!(Level.current is Editor))
             {
-                float num = wide.value;
-                float num2 = num * 0.5625f;
-                foreach (QuadLaserBullet q in Level.CheckRectAll<QuadLaserBullet>(position + new Vec2((0f - num) / 2f, (0f - num2) / 2f), position + new Vec2(num / 2f, num2 / 2f)))
+                foreach (QuadLaserBullet q in Level.CheckRectAll<QuadLaserBullet>(
+                    position - new Vec2(Width / 2f, Height / 2f),
+                    position + new Vec2(Width / 2f, Height / 2f)))
                 {
                     Level.Remove(q);
                 }
@@ -41,8 +45,12 @@ namespace DuckGame
 
         public override void Draw()
         {
-            wide.value = Size.value;
             base.Draw();
+            if (!Editor.editorDraw && Level.current is Editor)
+            {
+                Graphics.DrawRect(position - new Vec2(Width / 2f, Height / 2f),
+                    position + new Vec2(Width / 2f, Height / 2f), Color.OrangeRed * 0.5f);
+            }
         }
     }
 }
