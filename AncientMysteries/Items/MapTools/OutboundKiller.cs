a57556b9f3c162eefd11d6dd0ff9463d536ba37e
TheLostSoul <30169665+Ghogm4@users.noreply.github.com>
2021-07-21 11:17:23 +0800

    a

--- AncientMysteries/Items/MapTools/OutboundKiller.cs
+++ AncientMysteries/Items/MapTools/OutboundKiller.cs
@@ -4,8 +4,8 @@ namespace AncientMysteries.Items
 {
     [EditorGroup(group_MapTools_Gameplay)]
     [MetaImage(tex_MapTools_Swirl)]
-    [MetaInfo(Lang.english, "Outbound Killer", "Works well with Fixed Camera")]
-    [MetaInfo(Lang.schinese, null, "与固定相机完美配合")]
+    [MetaInfo(Lang.english, "Outbound Killer", "Works well with Fixed Camera.")]
+    [MetaInfo(Lang.schinese, null, "与固定相机完美配合。")]
     [MetaType(MetaType.MapTools)]
     public partial class OutboundKiller : AMMapToolGameplay
     {
@@ -13,7 +13,7 @@ namespace AncientMysteries.Items
         {
             this.ReadyToRun(tex_MapTools_Swirl);
             _editorName = "Outbound Killer";
-            editorTooltip = "Works well with Fixed Camera";
+            editorTooltip = "Works well with Fixed Camera.";
             _visibleInGame = false;
         }
 
@@ -22,9 +22,12 @@ namespace AncientMysteries.Items
             if (!(Level.current is Editor))
             {
                 foreach (Duck d in Level.current.things[typeof(Duck)])
-                if (d.y > Level.current.camera.bottom || d.y < Level.current.camera.top || d.x > Level.current.camera.right || d.x < Level.current.camera.left)
                 {
+                    Vec2 pos = d.ragdoll?.part1 is RagdollPart ragdollPart ? ragdollPart.position : d.position;
+                    if (pos.y > Level.current.camera.bottom || pos.y + 10 < Level.current.camera.top || pos.x > Level.current.camera.right || pos.x < Level.current.camera.left)
+                    {
                         d.Destroy(new DTCrush(d));
+                    }
                 }
             }
             base.Update();
