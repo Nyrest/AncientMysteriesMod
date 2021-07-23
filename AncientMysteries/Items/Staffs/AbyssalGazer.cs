0eac2cf3731c96d6ab91b8d9a53f8149b99d0755
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-23 21:52:09 +0800

    upload

--- AncientMysteries/Items/Staffs/AbyssalGazer.cs
+++ AncientMysteries/Items/Staffs/AbyssalGazer.cs
@@ -2,13 +2,14 @@
 {
     [EditorGroup(group_Guns_Staffs)]
     [MetaImage(tex_Staff_AbyssalGazer)]
-    [MetaInfo(Lang.english, "Abyssal Gazer", "desc")]
-    [MetaInfo(Lang.schinese, "", "")]
+    [MetaInfo(Lang.english, "Abyssal Gazer", "The Hellfire is swaying on the staff that shrouded in the darkness of sins.")]
+    [MetaInfo(Lang.schinese, "深渊凝视者", "地狱之业火摇曳于被无尽暗黑笼罩的罪恶之杖")]
     [MetaType(MetaType.Magic)]
     public partial class AbyssalGazer : AMStaff
     {
         public AbyssalGazer(float xval, float yval) : base(xval, yval)
         {
+            this.ReadyToRun(tex_Staff_AbyssalGazer);
         }
     }
 }
