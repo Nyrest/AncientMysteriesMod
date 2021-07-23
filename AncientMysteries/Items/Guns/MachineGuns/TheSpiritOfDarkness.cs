68c3ddc7153dfee16d705a76fa3f57ceda069010
TheLostSoul <30169665+Ghogm4@users.noreply.github.com>
2021-07-23 22:05:21 +0800

    tsod

--- AncientMysteries/Items/Guns/MachineGuns/TheSpiritOfDarkness.cs
+++ AncientMysteries/Items/Guns/MachineGuns/TheSpiritOfDarkness.cs
@@ -15,6 +15,29 @@ namespace AncientMysteries.Items
     {
         public TheSpiritOfDarkness(float xval, float yval) : base(xval, yval)
         {
+            this.ReadyToRun(tex_Gun_TheSpiritOfDarkness);
+            ammo = 1;
+            _ammoType = new AT_Shadow()
+            {
+                range = 360f,
+                accuracy = 0.7f,
+                penetration = 1f
+            };
+        }
+
+        public override void OnHoldAction()
+        {
+            base.OnHoldAction();
+            TheSpiritOfDarkness_ThingBullet b1 = new(barrelPosition + new Vec2(3,10), new Vec2(3, 0), duck, true);
+            TheSpiritOfDarkness_ThingBullet b2 = new(barrelPosition + new Vec2(3, 10), new Vec2(3, 0), duck, false) ;
+            Level.Add(b1);
+            Level.Add(b2);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
         }
     }
 }
