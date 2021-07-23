b3169223801ac7a4ffac19b97c1eb9c74ec3c4c9
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2021-07-23 22:05:21 +0800

    tsod

---
+++ AncientMysteries/Items/Guns/MachineGuns/TheSpiritOfDarkness.ThingBullet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    public partial class TheSpiritOfDarkness_ThingBullet : AMThingBulletLinar
+    {
+        public bool _goingUp = false;
+        public TheSpiritOfDarkness_ThingBullet(Vec2 pos, Vec2 initSpeed, Duck safeDuck, bool goingUp) : base(pos, 320, 2, initSpeed, safeDuck)
+        {
+            BulletTailColor = Color.Purple;
+            this.ReadyToRun(tex_Bullet_TSOD);
+            _goingUp = goingUp;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (_goingUp) y += (float)Math.Sin(2*x) * 3;
+            else y += -(float)Math.Sin(2 * x) * 3;
+        }
+    }
+}
