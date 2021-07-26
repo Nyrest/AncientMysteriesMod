ef13d4cda815d8b51be9f575c1f562f95d5c181b
TheLostSoul <30169665+Ghogm4@users.noreply.github.com>
2021-07-26 22:44:32 +0800

    a

--- AncientMysteries/Items/Staffs/BloodyMarquis.Crystal.cs
+++ AncientMysteries/Items/Staffs/BloodyMarquis.Crystal.cs
@@ -31,7 +31,7 @@ namespace AncientMysteries.Items
             {
                 fired = true;
                 #region Do Fire
-                var bullet = new BloodyMarquis_ThingBullet(position, 800, 1, GetBulletVecDeg(-fireAngleDegree, bulletSpeed), safeDuck);
+                var bullet = new BloodyMarquis_ThingBullet(position, 800, 1, GetBulletVecDeg(-fireAngleDegree, bulletSpeed) * 0.1f, safeDuck);
                 Level.Add(bullet);
                 #endregion
             }
--- AncientMysteries/Items/Staffs/BloodyMarquis.ThingBullet.cs
+++ AncientMysteries/Items/Staffs/BloodyMarquis.ThingBullet.cs
@@ -7,10 +7,12 @@ using System.Threading.Tasks;
 namespace AncientMysteries.Items{
     public class BloodyMarquis_ThingBullet : AMThingBulletLinar
     {
+        public override float CalcBulletAngleRadian(Vec2 speed) => base.CalcBulletAngleRadian(speed) + 1.56f;
         public BloodyMarquis_ThingBullet(Vec2 pos, float bulletRange, float bulletPenetration, Vec2 initSpeed, Duck safeDuck) : base(pos, bulletRange, bulletPenetration, initSpeed, safeDuck)
         {
             this.ReadyToRun(tex_Bullet_BloodyDagger);
             BulletTailColor = Color.DarkRed;
+            alpha = 0f;
         }
 
         public override void Update()
@@ -23,6 +25,8 @@ namespace AncientMysteries.Items{
                 
                 
             }
+            speed *= 1.04f;
+            MathHelper.Min(alpha += 0.04f, 1);
         }
     }
 }
