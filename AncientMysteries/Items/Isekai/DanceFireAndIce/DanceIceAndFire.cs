66c42e043316c3d82abffbfa9f3c9f4f6145ec82
Erstori <16686147+Erstori@users.noreply.github.com>
2021-07-30 21:20:24 +0800

    update perfunctorily

---
+++ AncientMysteries/Items/Isekai/DanceFireAndIce/DanceIceAndFire.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    [EditorGroup(group_Guns_Isekai)]
+    [MetaImage(tex_Gun_IceFireContainer)]
+    [MetaInfo(Lang.english, "Dance Ice And Fire", "desc")]
+    [MetaInfo(Lang.schinese, "", "")]
+    [MetaType(MetaType.Gun)]
+    partial class DanceIceAndFire : AMHoldable
+    {
+        public DanceIceAndFire(float xpos, float ypos) : base(xpos, ypos)
+        {
+            this.ReadyToRun(tex_Gun_IceFireContainer);
+        }
+    }
+}
