42991ed407a232317e777b07155475904d12d83a
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-30 22:51:30 +0800

    fix

--- AncientMysteries/Items/Equipments/Interstellar Armor Set/InterstellarVisage.cs
+++ AncientMysteries/Items/Equipments/Interstellar Armor Set/InterstellarVisage.cs
@@ -15,7 +15,7 @@ namespace AncientMysteries.Items
     {
         public InterstellarVisage(float xpos, float ypos) : base(xpos, ypos)
         {
-            // Not using ReadyToRun because we are going to use vanilla collide box and cropping size
+            // Not using ReadyToRun because we are going to use vanilla collide box and crop size
             graphic = _pickupSprite = this.ModSprite(tex_Equipment_InterstellarVisage_Pickup);
             _sprite = this.ModSpriteWithFrames(tex_Equipment_InterstellarVisage, 32, 32);
             CanCrush = false;
