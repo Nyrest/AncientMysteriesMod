62c000d3f79a961cba30aece9cd526ec27d3c557
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2021-07-30 22:39:57 +0800

    eq

---
+++ AncientMysteries/Items/Equipments/Interstellar Armor Set/InterstellarSuit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    [EditorGroup(group_Equipments)]
+    [MetaImage(tex_Equipment_InterstellarSuit_Pickup)]
+    [MetaInfo(Lang.english, "Interstellar Suit", "desc")]
+    [MetaInfo(Lang.schinese, "", "")]
+    [MetaType(MetaType.Equipment)]
+    public partial class InterstellarSuit : AMChestPlate
+    {
+        public InterstellarSuit(float xpos, float ypos) : base(xpos, ypos)
+        {
+            _pickupSprite = this.ReadyToRun(tex_Equipment_InterstellarSuit_Pickup);
+            _sprite = this.ReadyToRunWithFrames(tex_Equipment_InterstellarSuit_Anim, 32, 32);
+            _spriteOver = this.ReadyToRunWithFrames(tex_Equipment_InterstellarSuit_AnimOver,32,32);
+            CanCrush = false;
+        }
+    }
+}
