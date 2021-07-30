d6c565d963016e1dfd9cbdd42dec6204d5a2a111
TheLostSoul <30169665+Ghogm4@users.noreply.github.com>
2021-07-30 22:39:57 +0800

    eq

---
+++ AncientMysteries/Items/Equipments/Interstellar Armor Set/InterstellarBoots.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    [EditorGroup(group_Equipments)]
+    [MetaImage(tex_Equipment_InterstellarBoots_Pickup)]
+    [MetaInfo(Lang.english, "Interstellar Boots", "desc")]
+    [MetaInfo(Lang.schinese, "", "")]
+    [MetaType(MetaType.Equipment)]
+    public partial class InterstellarBoots : AMBoots
+    {
+        public InterstellarBoots(float xpos, float ypos) : base(xpos, ypos)
+        {
+            _pickupSprite = this.ReadyToRun(tex_Equipment_InterstellarBoots_Pickup);
+            _sprite = this.ReadyToRunWithFrames(tex_Equipment_InterstellarBoots, 32, 32);
+            CanCrush = false;
+        }
+    }
+}
---
+++ AncientMysteries/Items/Equipments/Interstellar Armor Set/InterstellarVisage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    [EditorGroup(group_Equipments)]
+    [MetaImage(tex_Equipment_InterstellarVisage_Pickup)]
+    [MetaInfo(Lang.english, "Interstellar Visage", "desc")]
+    [MetaInfo(Lang.schinese, "", "")]
+    [MetaType(MetaType.Equipment)]
+    public partial class InterstellarVisage : AMHelmet
+    {
+        public InterstellarVisage(float xpos, float ypos) : base(xpos, ypos)
+        {
+            _pickupSprite = this.ReadyToRun(tex_Equipment_InterstellarVisage_Pickup);
+            _sprite = this.ReadyToRunWithFrames(tex_Equipment_InterstellarVisage, 32, 32);
+            CanCrush = false;
+        }
+    }
+}
