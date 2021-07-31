21743babddc6a109bc67837b1de0a42a0ee64639
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-07-31 09:20:43 +0800

    Rebound Support For ThingBullet

--- AncientMysteries/AmmoTypes/_AT_ThingBulletSimulation.cs
+++ AncientMysteries/AmmoTypes/_AT_ThingBulletSimulation.cs
@@ -3,14 +3,32 @@
     /// <summary>
     /// Not designed to network sync
     /// </summary>
-    public class AT_ThingBulletSimulation : AMAmmoType
+    public class ThingBulletSimulation_AmmoType : AMAmmoType
     {
-        public AT_ThingBulletSimulation()
+        public ThingBulletSimulation_AmmoType()
         {
             penetration = 0;
             range = 2;
             bulletSpeed = range;
-            this.canBeReflected = false;
+            this.canBeReflected = true;
+            bulletType = typeof(ThingBulletSimulation_Bullet);
         }
+
+        #region Bullet
+        public class ThingBulletSimulation_Bullet : AMBullet
+        {
+            public AMThingBulletBase callback;
+
+            public ThingBulletSimulation_Bullet(float xval, float yval, AmmoType type, float ang = -1, Thing owner = null, bool rbound = false, float distance = -1, bool tracer = false, bool network = true) : base(xval, yval, type, ang, owner, rbound, distance, tracer, network)
+            {
+
+            }
+
+            protected override void Rebound(Vec2 pos, float dir, float rng)
+            {
+                callback?.LegacyRebound(pos, dir, rng);
+            }
+        }
+        #endregion
     }
 }
