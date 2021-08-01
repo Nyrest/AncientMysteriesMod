7cd30a191cd2abd7d7f5d52c5fcae377197fe7e4
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-08-01 20:41:08 +0800

    upd

--- AncientMysteries/Items/Melee/PowerFist.cs
+++ AncientMysteries/Items/Melee/PowerFist.cs
@@ -9,8 +9,8 @@ namespace AncientMysteries.Items
 {
     [EditorGroup(group_Guns_Melees)]
     [MetaImage(tex_Melee_PowerFist)]
-    [MetaInfo(Lang.Default, "Power Fist", "desc")]
-    [MetaInfo(Lang.schinese, "", "")]
+    [MetaInfo(Lang.Default, "Power Fist", "♫ <Deja Vu>")]
+    [MetaInfo(Lang.schinese, "能量拳套", "逮虾户!!")]
     [MetaType(MetaType.Melee)]
     public partial class PowerFist : AMHoldable
     {
