ef8f92b357ca230171d54e0240cba224cf603e4b
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2021-08-01 22:23:49 +0800

    fuck ef

--- AncientMysteries/Items/Melee/EternalFlame.Bullet.cs
+++ AncientMysteries/Items/Melee/EternalFlame.Bullet.cs
@@ -6,34 +6,12 @@
         {
         }
 
-        public override void OnCollide(Vec2 pos, Thing t, bool willBeStopped)
-        {
-            base.OnCollide(pos, t, willBeStopped);
-            if (willBeStopped)
-            {
-                ExplosionPart ins = new(pos.x, pos.y, true);
-                ins.xscale *= 0.7f;
-                ins.yscale *= 0.7f;
-                Level.Add(ins);
-                SFX.Play("explode", 0.7f, Rando.Float(-0.7f, -0.5f), 0f, false);
-                Thing bulletOwner = owner;
-                IEnumerable<MaterialThing> things = Level.CheckCircleAll<MaterialThing>(pos, 14f);
-                foreach (MaterialThing t2 in things)
-                {
-                    if (t2 != bulletOwner)
-                    {
-                        t2.Destroy(new DTIncinerate(this));
-                    }
-                }
-            }
-        }
-
         public override void Removed()
         {
             base.Removed();
             ExplosionPart ins = new(travelEnd.x, travelEnd.y, true);
-            ins.xscale *= 0.7f;
-            ins.yscale *= 0.7f;
+            ins.xscale *= 1.4f;
+            ins.yscale *= 1.4f;
             Level.Add(ins);
             SFX.Play("explode", 0.7f, Rando.Float(-0.7f, -0.5f), 0f, false);
             Thing bulletOwner = owner;
