1159576b7719053f6261f7b0ebc0609bda081710
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2021-08-01 23:23:22 +0800

    a

--- AncientMysteries/Items/Staffs/PrimordialLibram.ThingBullet.Flower.cs
+++ AncientMysteries/Items/Staffs/PrimordialLibram.ThingBullet.Flower.cs
@@ -1,8 +1,11 @@
-﻿namespace AncientMysteries.Items
+﻿
+
+namespace AncientMysteries.Items
 {
     public class PrimordialLibram_ThingBullet_Flower : AMThingBulletLinar
     {
-        public PrimordialLibram_ThingBullet_Flower(Vec2 pos, Vec2 initSpeed, Duck safeDuck) : base(pos, 300, 3, initSpeed, safeDuck)
+        public Waiter w = new(10);
+        public PrimordialLibram_ThingBullet_Flower(Vec2 pos, Vec2 initSpeed, Duck safeDuck) : base(pos, 300, 3f, initSpeed, safeDuck)
         {
             this.ReadyToRun(tex_Bullet_Flower);
             angleDegrees = Rando.Float(0, 360);
@@ -28,6 +31,15 @@
             base.Update();
             angle += 30;
             MathHelper.Min(alpha += 0.04f, 1);
+        }
+
+        public override bool BulletCanHit(MaterialThing thing)
+        {
+            if (thing is Boring3)
+            {
+                return false;
+            }
+            return base.BulletCanHit(thing);
         }
     }
 }
