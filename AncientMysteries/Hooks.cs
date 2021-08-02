6f2c83a19d64c05cd31eddbdd996cc1923d7a1fa
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-08-02 23:08:26 +0800

    hell fucking yeah

--- AncientMysteries/Hooks.cs
+++ AncientMysteries/Hooks.cs
@@ -1,12 +1,44 @@
+using DuckGame;
+using HarmonyLib;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace AncientMysteries
 {
+    public static class LevelPostDraw
+    {
+        private static readonly List<Action> drawQueue = new();
+
+        static LevelPostDraw()
+        {
+            Hooks.LevelPostDraw += Flush;
+        }
+
+        public static void Draw(Action action)
+        {
+            drawQueue.Add(action);
+        }
+
+        public static void Flush(Level level)
+        {
+            if (Level.current is null) return;
+            Graphics.screen.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, level.camera.getMatrix());
+            for (int i = 0; i < drawQueue.Count; i++)
+            {
+                drawQueue[i]();
+            }
+            Graphics.screen.End();
+            drawQueue.Clear();
+        }
+    }
+
     public static class Hooks
     {
         public static List<Thing> removedThings = new(256);
 
         public static bool _initialized;
+
+        public static readonly Harmony harmony = new("FuckingAncientMysteriesMod");
 
         public static void Initialize()
         {
@@ -15,16 +47,25 @@ namespace AncientMysteries
             //
             (typeof(Game).GetField("updateableComponents", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(MonoMain.instance) as List<IUpdateable>).Add(new HookUpdate());
             (typeof(Game).GetField("drawableComponents", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(MonoMain.instance) as List<IDrawable>).Add(new HookDraw());
+
+            harmony.Patch(AccessTools.Method(typeof(Level), nameof(Level.DoDraw)), null, new HarmonyMethod(AccessTools.Method(typeof(Hooks), nameof(DoDraw))));
         }
 
         public static event Action OnUpdate;
 
         public static event Action OnDraw;
 
+        public static event Action<Level> LevelPostDraw;
+
+        public static void DoDraw(Level __instance)
+        {
+            LevelPostDraw?.Invoke(__instance);
+        }
+
         private sealed class HookDraw : IDrawable
         {
             public bool Visible => true;
-            public int DrawOrder => 0;
+            public int DrawOrder => int.MaxValue;
 
 #pragma warning disable CS0067 // Unreachable code detected
 
