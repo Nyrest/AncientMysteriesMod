bf7ac5f22c39b21902c425560a11495681663bf0
TheLostSoul <30169665+Ghogm4@users.noreply.github.com>
2021-08-02 22:10:09 +0800

    pl

---
+++ AncientMysteries/Items/Guns/Snipers/PermafrostLance.ThingBullet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    public class PermafrostLance_ThingBullet : AMThingBulletLinar
+    {
+        public PermafrostLance_ThingBullet(Vec2 pos, Vec2 initSpeed, Duck safeDuck) : base(pos, 1750, 2, initSpeed, safeDuck)
+        {
+            this.ReadyToRun(tex_Bullet_PermafrostBullet);
+        }
+
+        public override ColorTrajectory GetTrajectory()
+        {
+            return null;
+        }
+
+        public override void Removed()
+        {
+            PermafrostExplosion exp = new(x, y,false);
+            Level.Add(exp);
+            SFX.PlaySynchronized("glassBreak",1,1);
+            base.Removed();
+        }
+    }
+}
