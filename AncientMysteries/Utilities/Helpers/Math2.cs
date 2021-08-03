65b3df79a968b4487e2b69b6a133305af3735b0d
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-08-03 21:38:50 +0800

    upd

--- AncientMysteries/Utilities/Helpers/Math2.cs
+++ AncientMysteries/Utilities/Helpers/Math2.cs
@@ -18,7 +18,7 @@
 
         public static Vec2 GetBulletVecDeg(float degress, float speed = 1f, float speedVariable = 0, float accuracy = 1f)
         {
-            var accuracyLossDeg = Rando.Float(360 * accuracy);
+            var accuracyLossDeg = Rando.Float(360 * (1 - accuracy));
             return Maths.AngleToVec(Maths.DegToRad(degress + accuracyLossDeg)) * (speed + Rando.Float(speedVariable).RandomNegative());
         }
     }
