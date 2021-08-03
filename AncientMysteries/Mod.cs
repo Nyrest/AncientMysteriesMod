1b9d4cfbc1062fcf1d1c1ecb72f8a6194ea3ddd6
Erstori <16686147+Erstori@users.noreply.github.com>
2021-08-03 21:29:48 +0800

    upd

--- AncientMysteries/Mod.cs
+++ AncientMysteries/Mod.cs
@@ -8,16 +8,16 @@ namespace AncientMysteries
         protected override unsafe void OnPreInitialize()
         {
             base.OnPreInitialize();
-            Hooks.Initialize();
-            if (Debugger.IsAttached)
-            {
-                MonoMain.modDebugging = true;
-            }
             var oldMethod = typeof(Program).GetMethod("ModResolve").MethodHandle;
             var newMethod = typeof(LightweightDependencyResolver).GetMethod("ModResolve").MethodHandle;
             RuntimeHelpers.PrepareMethod(oldMethod);
             RuntimeHelpers.PrepareMethod(newMethod);
             *((int*)oldMethod.Value.ToPointer() + 2) = *((int*)newMethod.Value.ToPointer() + 2);
+            Hooks.Initialize();
+            if (Debugger.IsAttached)
+            {
+                MonoMain.modDebugging = true;
+            }
         }
 
         protected override void OnPostInitialize()
