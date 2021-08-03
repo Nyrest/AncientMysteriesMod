3b2672619fde2ea830972296f1286e729790eaef
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-08-03 17:02:12 +0800

    upd

---
+++ AncientMysteries/Items/Guns/MachineGuns/Refrigerator.Backpack.cs
@@ -0,0 +1,18 @@
+﻿namespace AncientMysteries.Items
+{
+    public class Refrigerator_Backpack : Equipment
+    {
+        public StateBinding refrigeratorBinding = new StateBinding(nameof(refrigerator));
+        public Refrigerator refrigerator;
+
+        public Refrigerator_Backpack(float xpos, float ypos, Refrigerator refrigerator) : base(xpos, ypos)
+        {
+            this.ReadyToRun(tex_Equipment_Refrigerator);
+            _isArmor = false;
+            enablePhysics = false;
+            this.refrigerator = refrigerator;
+        }
+
+        public override bool Destroy(DestroyType type = null) => false;
+    }
+}
