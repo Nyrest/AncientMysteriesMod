d23cd38b6683f89d8f5d2b14358a0db2708c0b16
Erstori <16686147+Erstori@users.noreply.github.com>
2021-08-03 22:03:46 +0800

    upd

---
+++ AncientMysteries/Items/Guns/MachineGuns/Refrigerator.AmmoType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    public class Refrigerator_AmmoType : AMAmmoType
+    {
+        public Refrigerator_AmmoType()
+        {
+            range = 500;
+            rangeVariation = 30;
+            bulletSpeed = 23;
+            speedVariation = 2;
+            sprite = tex_Bullet_Refrigerator.ModSprite();
+            HideTail();
+        }
+    }
+}
