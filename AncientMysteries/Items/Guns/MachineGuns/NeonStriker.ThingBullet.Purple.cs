c85c02ef9ecb62558fc2b06e47ac6067f0abbb9a
TheLostSoul <30169665+Ghogm4@users.noreply.github.com>
2021-08-04 20:02:58 +0800

    Neon Striker!

---
+++ AncientMysteries/Items/Guns/MachineGuns/NeonStriker.ThingBullet.Purple.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    public class NeonStriker_ThingBullet_Purple : AMThingBulletLinar
+    {
+        public Waiter w = new(2);
+
+        public bool _canMultiply;
+        public NeonStriker_ThingBullet_Purple(Vec2 pos, Vec2 initSpeed, Duck safeDuck, bool canMultiply) : base(pos, 1000, 1, initSpeed, safeDuck)
+        {
+            this.ReadyToRun(tex_Bullet_NeonLightPurple);
+            _canMultiply = canMultiply;
+            hasGravity = true;
+            gravityIncrement = 0.2f;
+            maxGravity = 1f;
+        }
+
+        public override void Update()
+        {
+            if (!_canMultiply)
+            {
+                alpha -= 0.1f;
+                if (alpha <= 0)
+                {
+                    Level.Remove(this);
+                }
+            }
+            if (w.Tick() && _canMultiply)
+            {
+                NeonStriker_ThingBullet_Purple b = new(position, new Vec2(0.001f,0), BulletSafeDuck, false);
+                Level.Add(b);
+            }
+            base.Update();
+        }
+        public override ColorTrajectory GetTrajectory()
+        {
+            return null;
+        }
+    }
+}
--- AncientMysteries/Items/Guns/Snipers/PermafrostLance.ThingBulletChargedSmall.cs
+++ AncientMysteries/Items/Guns/Snipers/PermafrostLance.ThingBulletChargedSmall.cs
@@ -8,7 +8,8 @@ namespace AncientMysteries.Items
 {
     public class PermafrostLance_ThingBulletChargedSmall : AMThingBulletLinar
     {
-        public PermafrostLance_ThingBulletChargedSmall(Vec2 pos, Vec2 initSpeed, Duck safeDuck) : base(pos, 150, 1, initSpeed, safeDuck)
+        public int aliveTime = 0;
+        public PermafrostLance_ThingBulletChargedSmall(Vec2 pos, Vec2 initSpeed, Duck safeDuck) : base(pos, 15000, 1, initSpeed, safeDuck)
         {
             this.ReadyToRun(tex_Bullet_PermafrostBulletChargedSmall);
             xscale = yscale = 1.3f;
@@ -17,6 +18,20 @@ namespace AncientMysteries.Items
         public override ColorTrajectory GetTrajectory()
         {
             return null;
+        }
+
+        public override void Update()
+        {
+            aliveTime++;
+            if (aliveTime >= 35)
+            {
+                alpha -= 0.2f;
+                if (alpha <= 0)
+                {
+                    Level.Remove(this);
+                }
+            }
+            base.Update();
         }
     }
 }
