b5577e2d2672ee61e6ed2c092e5775670919b810
Erstori <16686147+Erstori@users.noreply.github.com>
2021-08-04 11:57:10 +0800

    Epitaph

---
+++ AncientMysteries/Items/Props/Epitaph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+    [EditorGroup(group_Props)]
+    [MetaImage(tex_Holdable_Epitaph)]
+    [MetaInfo(Lang.Default, "Epitaph", "todo")]
+    [MetaInfo(Lang.schinese, "", "")]
+    [MetaType(MetaType.Props)]
+    public partial class Epitaph : AMHoldable
+    {
+        public Epitaph(float xpos, float ypos) : base(xpos, ypos)
+        {
+            this.ReadyToRun(tex_Holdable_Epitaph);
+        }
+
+
+    }
+}
