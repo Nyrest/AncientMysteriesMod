7b8db9bf387c78c4d2b9327a0056725452dc82f1
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-08-04 17:26:04 +0800

    upd

---
+++ AncientMysteries/Items/_Bases/AMBuff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncientMysteries.Items
+{
+
+    public abstract class AMBuff : AMThing
+    {
+        public StateBinding duckBinding = new(nameof(duck));
+        public Duck duck { get; private set; }
+
+        protected AMBuff() : base(0, 0)
+        {
+        }
+    }
+}
