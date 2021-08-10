4f49c4fdfafbbc3ff7ba348394e67f6eca8880cc
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-08-10 23:47:30 +0800

    Add Dark Wings

---
+++ AncientMysteries/Items/Equipments/Back/DarkWings.cs
@@ -0,0 +1,19 @@
+﻿namespace AncientMysteries.Items
+{
+    [EditorGroup(group_Equipments)]
+    [MetaImage(tex_Equipment_DarkWings, 43, 31, 1)]
+    [MetaInfo(Lang.Default, "Dark Wings", "")]
+    [MetaInfo(Lang.schinese, "漆黑之翼", "")]
+    [MetaType(MetaType.Equipment)]
+    public partial class DarkWings : AMEquipmentWings
+    {
+        public DarkWings(float xpos, float ypos) : base(xpos, ypos)
+        {
+            _wingsSpriteMap = this.ReadyToRunWithFrames(tex_Equipment_DarkWings, 43, 31);
+            _wingsSpriteMap.AddAnimation("loop", 0.2f, true, 1, 2, 3, 2);
+            _wingsSpriteMap.AddAnimation("idle", 1f, true, 0);
+            wearOffset = new(-0.5f, 1);
+            _equippedDepth = -12;
+        }
+    }
+}
