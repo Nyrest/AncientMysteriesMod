0585b6be3bb0f4774d285234bf67a0f2b5934028
Nyrest <16686147+Nyrest@users.noreply.github.com>
2021-08-12 12:56:53 +0800

    Update Dynamic Logo

--- AncientMysteries/Utilities/ExtensionMethods.cs
+++ AncientMysteries/Utilities/ExtensionMethods.cs
@@ -11,5 +11,16 @@
         {
             return thing.offDir == 1 ? 0 : 180;
         }
+
+        /// <summary>
+        /// Use this when collisionSize different with frame size
+        /// </summary>
+        /// <param name="w"></param>
+        /// <param name="h"></param>
+        public static void SetBox(this Thing thing, float w, float h)
+        {
+            thing.collisionOffset = -(thing.center = new Vec2(w / 2, h / 2));
+            thing.collisionSize = new Vec2(w, h);
+        }
     }
 }
