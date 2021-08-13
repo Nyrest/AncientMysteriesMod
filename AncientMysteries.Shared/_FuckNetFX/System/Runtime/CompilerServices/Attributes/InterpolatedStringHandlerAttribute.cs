a10e1e2a4440bdd626e348db18a81094a9c5ec97
Erstori <16686147+Erstori@users.noreply.github.com>
2021-08-14 00:45:00 +0800

    Introduce C# 10 String Interpolation Handler

---
+++ AncientMysteries.Shared/_FuckNetFX/System/Runtime/CompilerServices/Attributes/InterpolatedStringHandlerAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    public sealed class InterpolatedStringHandlerAttribute : Attribute
+    {
+    }
+}
